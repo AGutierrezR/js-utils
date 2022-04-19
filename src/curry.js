9c9281f076843865f528cfa741175a7896a12012
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2022-04-19 15:24:17 +0200

    Improve `curry` function and tests
    _arity function was use to properly reports the legth of the curriend function

--- src/curry.js
+++ src/curry.js
@@ -1,13 +1,26 @@
-export function curry(fn, initialArgs = []) {
-  if (typeof fn !== 'function') {
-    throw new Error('Expected a function to curried')
-  }
+import { _arity } from './internals/_arity'
 
+/**
+ * Returns a curried equivalent of the provided function.
+ *
+ * @param {Function} fn - Function to curry
+ * @param {Array} initialArgs - Initial arguments to pass to the function
+ * @returns {Function} - A new, curried function
+ */
+export function curry(fn, initialArgs = []) {
+  if (typeof fn !== 'function')
+    throw new Error('Expected a function to curried')
+
+  return _arity(fn.length, _curry(fn, initialArgs))
+}
+
+function _curry(fn, received = []) {
   return function curried(...args) {
-    return ((rest) =>
-      rest.length >= fn.length ? fn.apply(this, rest) : curry(fn, rest))([
-      ...initialArgs,
-      ...args,
-    ])
+    const _args = [...received, ...args]
+    const argsLeft = fn.length - _args.length
+
+    return _args.length >= fn.length
+      ? fn.apply(this, _args)
+      : _arity(argsLeft, _curry(fn, _args))
   }
 }
--- test/curry.test.js
+++ test/curry.test.js
@@ -16,7 +16,25 @@ describe('curry', () => {
     expect(h(3)).toBe(18)
   })
 
-  it.todo('Should properly reports the length of the curriend function')
+  it('Should allows further currying of a curried function', () => {
+    const f = curry((a, b, c, d) => a + b + c + d)
+    const g = f(12)
+    expect(g(3, 6, 2)).toBe(23)
+    const h = g(3)
+    expect(h(6, 2)).toBe(23)
+    expect(g(3, 6)(2)).toBe(23)
+  })
+
+  it('Should properly reports the length of the curriend function', () => {
+    const f = curry((a, b, c, d) => a + b + c + d)
+    expect(f.length).toBe(4)
+    const g = f(12)
+    expect(g.length).toBe(3)
+    const h = g(1)
+    expect(h.length).toBe(2)
+    const i = h(2)
+    expect(i.length).toBe(1)
+  })
 
   it('Should preserve the context', () => {
     const context = { x: 10 }
